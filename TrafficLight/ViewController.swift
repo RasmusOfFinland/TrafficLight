d9f291acdc97c3bf773a2bab41c1bbd4818842a0
Vyacheslav <wooly_mormut@mail.ru>
2022-08-14 03:32:58 +0300

    end

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var changeColorButton: UIButton!
+    
+    var colorState = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let viewsOfTrafficLightColor = [
+            redLightView, yellowLightView, greenLightView
+        ]
+        
+        for view in viewsOfTrafficLightColor {
+            view?.alpha = 0.3
+            view?.layer.cornerRadius = view?.frame.height ?? 0 / 2
+            
+        }
+        
+       changeColorButton.layer.cornerRadius = 10
+        
+        }
+    
+    @IBAction func switchColorButtonAction() {
+        
+        changeColorButton.setTitle("Next", for: .normal)
+        
+        switch colorState {
+        case "red" :
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            colorState = "yellow"
+        case "yellow":
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            colorState = ""
+            
+        default:
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+            colorState = "red"
+            
+        }
+        
+        
     }
-
+    
 
 }
 
